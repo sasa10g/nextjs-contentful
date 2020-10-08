2f531f89982082f8f2d492f627b14abf24b2a517
Sasa <sasagvozdenovic@hotmail.com>
2020-10-08 23:42:04 +0200

    ff

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const client = require('contentful').createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 })
-
+ 
 function HomePage() {
   async function fetchEntries() {
     const entries = await client.getEntries()
